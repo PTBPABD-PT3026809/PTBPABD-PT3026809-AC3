2885fa2eb5379d9fd1142dc82eb173c7c670c3be
Herick <125935674+HerickYag@users.noreply.github.com>
2026-03-09 18:32:40 +0000

    criado arquivo de consulta das tabelas

---
+++ contagem_registros.sql
@@ -0,0 +1,43 @@
+-- Tabela: advisor
+SELECT COUNT(*) FROM dbo.advisor;
+-- Quantidade de registros: 2000
+
+-- Tabela: classroom
+SELECT COUNT(*) FROM dbo.classroom;
+-- Quantidade de registros: 30
+
+-- Tabela: course
+SELECT COUNT(*) FROM dbo.course;
+-- Quantidade de registros: 200
+
+-- Tabela: department
+SELECT COUNT(*) FROM dbo.department;
+-- Quantidade de registros: 20
+
+-- Tabela: instructor
+SELECT COUNT(*) FROM dbo.instructor;
+-- Quantidade de registros: 50
+
+-- Tabela: prereq
+SELECT COUNT(*) FROM dbo.prereq;
+-- Quantidade de registros: 100
+
+-- Tabela: section
+SELECT COUNT(*) FROM dbo.section;
+-- Quantidade de registros: 100
+
+-- Tabela: student
+SELECT COUNT(*) FROM dbo.student;
+-- Quantidade de registros: 2000
+
+-- Tabela: takes
+SELECT COUNT(*) FROM dbo.takes;
+-- Quantidade de registros: 30000
+
+-- Tabela: teaches
+SELECT COUNT(*) FROM dbo.teaches;
+-- Quantidade de registros: 100
+
+-- Tabela: time_slot
+SELECT COUNT(*) FROM dbo.time_slot;
+-- Quantidade de registros: 20
